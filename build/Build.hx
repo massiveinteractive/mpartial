c85b2947989b5f1fdb6b45562b0dac093d1e47a1
David Peek <david.peek@massiveinteractive.com>
2012-08-29 16:48:22 +1000

    Updated require -> invoke

--- build/Build.hx
+++ build/Build.hx
@@ -67,9 +67,9 @@ class Build extends mtask.core.BuildBase
 
 	@task function release()
 	{
-		require("clean");
-		require("test");
-		require("build haxelib", "build examples");
+		invoke("clean");
+		invoke("test");
+		invoke("build haxelib", "build examples");
 	}
 
 
